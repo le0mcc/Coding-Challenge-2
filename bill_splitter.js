33055690488509b8721aac15c2a2a8d80f9deeae
Leo McClellan <mcclellan2@usf.edu>
2024-09-08 13:59:28 -0400

    Implement and populate arrays for bills, tips, and totals

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,17 +1,31 @@
-// Bill Splitter Application
+// Bill Splitter Application Leo McClellan
 
 // Task 1: Calculate the tip with a ternary operator
-let bill = 20
-let tip = (bill >= 50 && bill <= 300) ? (bill * .15) : (bill * .2);
+let billTaskOne = 20;
+let tip = (billTaskOne >= 50 && billTaskOne <= 300) ? (billTaskOne * .15) : (billTaskOne * .2);
 
 // Task 2: Display the bill, tip, and total bill in the console
-let totalBill = bill + tip
-console.log(`The bill was $${bill}, the tip was $${tip}, and the total bill was $${totalBill}.`)
+let totalBillTaskOne = billTaskOne + tip;
+console.log(`The bill was $${billTaskOne}, the tip was $${tip}, and the total bill was $${totalBillTaskOne}.`);
 
 // Task 3: Create a function named calculateTip and test it
 function calculateTip(testBill) {
-    return (testBill >= 50 && testBill <= 300) ? (testBill * .15) : (testBill * .2); }
+    return (testBill >= 50 && testBill <= 300) ? (testBill * .15) : (testBill * .2) }
 let testBill = 100;
 let testTip = calculateTip(testBill);
-let testTotal = testBill + testTip
-console.log(`The bill was $${testBill}, the tip was $${testTip}, and the total was $${testTotal}.`)
+let testTotal = testBill + testTip;
+console.log(`The bill was $${testBill}, the tip was $${testTip}, and the total was $${testTotal}.`);
+
+// Task 4: Utilize arrays to show sets of data for bills and tips 
+// Task 5: Test this data set in browser
+// First set of given data
+let bills = [275,40,430];
+let tips = bills.map(bill => calculateTip(bill));
+let totals = bills.map((bill,index) => bill + tips[index]);
+console.log(`The totals are $${totals}.`);
+
+// Second set of given data
+let billsTwo = [125,555,44];
+let tipsTwo = billsTwo.map(bill => calculateTip(bill));
+let totalsTwo = billsTwo.map((bill,index) => bill + tipsTwo[index]);
+console.log(`The totals are $${totalsTwo}.`);
