1b8111ba7774859380b4c5cbf4af54545c33406a
Leo McClellan <mcclellan2@usf.edu>
2024-09-09 16:18:36 -0400

    update

--- bill_splitter.js
+++ bill_splitter.js
@@ -19,13 +19,13 @@ console.log(`The bill was $${testBill}, the tip was $${testTip}, and the total w
 // Task 4: Utilize arrays to show sets of data for bills and tips 
 // Task 5: Test this data set in browser
 // First set of given data
-let bills = [275,40,430];
+let bills = [275, 40, 430];
 let tips = bills.map(bill => calculateTip(bill));
 let totals = bills.map((bill,index) => bill + tips[index]);
-console.log(`The totals are $${totals}.`);
+console.log(`The totals for the first dataset are $${totals}.`);
 
 // Second set of given data
 let billsTwo = [125,555,44];
 let tipsTwo = billsTwo.map(bill => calculateTip(bill));
 let totalsTwo = billsTwo.map((bill,index) => bill + tipsTwo[index]);
-console.log(`The totals are $${totalsTwo}.`);
+console.log(`The totals for the second dataset are $${totalsTwo}.`);
